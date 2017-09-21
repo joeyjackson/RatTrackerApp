f66221f396129e1c3a5740229637e212d980fb5f
joeyjackson <joeyjackson4712@gmail.com>
2017-09-21 00:58:25 -0400

    added startscreen buttons, loginscreen activity and widgets

--- RatTrackerApp/app/src/main/java/com/lead/rattrackerapp/StartScreen.java
+++ RatTrackerApp/app/src/main/java/com/lead/rattrackerapp/StartScreen.java
@@ -1,17 +1,30 @@
 package com.lead.rattrackerapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartScreen extends AppCompatActivity {
+    Button loginButton;
+    Button signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+        loginButton = (Button) findViewById(R.id.loginButton);
+        signUpButton = (Button) findViewById(R.id.signUpButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartScreen.this, LogInScreen.class);
+                startActivity(intent);
+            }
+        });
 
 
 
-        
     }
 }
